f234705e98ba36a55ff50dc41fb0f5b94addec1d
DawoodBuilds <hm.davidhussain@gmail.com>
2025-12-03 17:34:06 +0500

    Configure Proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,17 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export default clerkMiddleware();
+export function proxy(request: NextRequest) {
+  const forbiddenPaths = ["/icon.png", "/og-image.png"];
+  const isForbiddenPath = forbiddenPaths.some((path) =>
+    request.nextUrl.pathname.startsWith(path)
+  );
+  if (isForbiddenPath) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+}
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
