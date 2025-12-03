3610bc9342c81118863381cca62c68ba440005b1
DawoodBuilds <hm.davidhussain@gmail.com>
2025-12-03 17:57:33 +0500

    Configure Proxy.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-const isForbiddenRoute = createRouteMatcher(["/icon.png", "/og-image.png"]);
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware((auth, req) => {
-  if (isForbiddenRoute(req)) {
-    const url = new URL("/dashboard", req.url);
-    return NextResponse.redirect(url);
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
